c7ba6d046845ddb56bbaf6ddd0b662af786ae4d3
sebastiankurowski <sebastian.kurowski@gmail.com>
2020-06-08 21:00:22 +0200

    wersja 0.9

--- src/kadry/Main.java
+++ src/kadry/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
+/*
         Pracownik pracownik1 = new Pracownik("Sebastian", "Kurowski", 5000, 'M', 1);
         System.out.println( pracownik1.toString() );
 
@@ -21,10 +21,54 @@ public class Main {
         System.out.println(pracownik1.czyPracujeWDziale(nrDzialu));
         System.out.println(pracownik2.czyPracujeWDziale(nrDzialu));
         System.out.println(pracownik3.czyPracujeWDziale(nrDzialu));
-        */
+*/
 
+        Scanner skaner = new Scanner(System.in);
         Kadry kadry = new Kadry();
-        kadry.dodajPracownikaInteraktywnie();
+        System.out.println("Witaj w programie obsługi Kadr!\nWybierz akcję:");
 
+        int wybranaAkcja = 1;
+
+        do {
+
+            System.out.println(
+                    "1 - dodajPracownikaInteraktywnie()\n" +
+                    "2 - sredniZarobek()\n" +
+                    "3 - sredniZarobek(int dzial)\n" +
+                    "4 - dajDzialy()\n" +
+                    "5 - pisz()\n" +
+                    "0 - zakończ\n");
+            wybranaAkcja = Integer.parseInt(skaner.nextLine());
+
+            switch (wybranaAkcja) {
+                case 1:
+                    kadry.dodajPracownikaInteraktywnie();
+                    break;
+                case 2:
+                    System.out.println("Średnie zarobki w całej firmie wynoszą " + kadry.sredniZarobek() + "\n\n");
+                    System.out.println("Wybierz kolejną akcję:\n");
+                    break;
+                case 3:
+                    System.out.println("Podaj numer działu dla którego mam zwrócić średnie zarobki:\n");
+                    int numerDzialu = Integer.parseInt(skaner.nextLine());
+                    kadry.sredniZarobek(numerDzialu);
+                    System.out.println("Średnie zarobki w dziale " + numerDzialu + " wynoszą: " + kadry.sredniZarobek(numerDzialu) + "\n\n");
+                    System.out.println("Wybierz kolejną akcję:\n");
+                    break;
+                case 4:
+                    System.out.println(kadry.dajDzialy());
+                    System.out.println("Wybierz kolejną akcję:\n");
+                    break;
+                case 5:
+                    kadry.pisz();
+                    System.out.println("Wybierz kolejną akcję:\n");
+                    break;
+                case 0:
+                    System.out.println("Zakończono działanie programu. Dziękujemy!\n");
+                default:
+                    System.out.println("Wprowadzono błędną wartość.\nSpróbuj ponownie:");
+            }
+        }
+        while (wybranaAkcja != 0);
     }
 }
--- src/kadry/kadry.java
+++ src/kadry/kadry.java
@@ -1,29 +1,40 @@
 package kadry;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Kadry{
 
     private Pracownik [] pracownicy;
     private int zatrudnienie;
+    private int limitZatrudnienia = 5;
+
 
     Kadry(){
         pracownicy = new Pracownik[100];
-        zatrudnienie = 0;
+        zatrudnienie = 3;
+        pracownicy[0] = new Pracownik("Sebastian", "Kurowski", 5000, 'M', 1);
+        pracownicy[1] = new Pracownik("Jakub", "Mrozowski", 4000.99, 'M', 2);
+        pracownicy[2] = new Pracownik("Adrian", "Palich", 4200, 'M', 2);
     }
 
     public void dodajPracownika(Pracownik pracownik){
 
-        if(zatrudnienie < 100){
+        if (zatrudnienie < limitZatrudnienia) {
             pracownicy[zatrudnienie] = pracownik;
             zatrudnienie++;
-
+            if (zatrudnienie == limitZatrudnienia) {
+                System.out.println("To był ostatni możliwy do przyjęcia pracownik.\n Zwiększ aktualny limit zatrudnienia: "
+                        + limitZatrudnienia + " jeśli chcesz zatrudnić więcej osób.");
+            }
+            System.out.println("Dodano pracownika nr: " + zatrudnienie + "\n");
+        }
+        else {
+            System.out.println("Osięgnięto limit pracowników, nie można dodać więcej! Operacja anulowana.");
         }
     }
 
     public void dodajPracownikaInteraktywnie(){
-
-        //Pracownik tmp = new Pracownik();
 
         Scanner myObj = new Scanner(System.in);
         System.out.println("Wprowadz imie: ");
@@ -72,14 +83,27 @@ class Kadry{
                     counter++;
                 }
             }
-            return suma / counter;
+            if (counter > 0) {
+                return suma / counter;
+            } else {
+                return 0.0;
+            }
         }else {
             return 0.0;
         }
     }
 
-
-    //public int[] dajDzialy(){
-    //    int[] listaDzialow = new int[20];
-    //}
+    public void pisz(){
+        for ( int i = 0; i < zatrudnienie; i++ ) {
+            pracownicy[i].toString();
+            System.out.println(pracownicy[i]);
+        }
+        System.out.println("Średnie zarobki w całej firmie wynoszą: " + this.sredniZarobek() + "\n\n");
+    }
+//TODO: dokonczyć dajDzialy żeby zwracało unikalne wartości z listy działów
+    public int[] dajDzialy(){
+        int[] listaDzialow = new int[20];
+        Arrays.stream(listaDzialow);
+        return listaDzialow;
+    }
 }
